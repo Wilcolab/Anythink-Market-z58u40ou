285ef18f641bcd9b782a17121dbf391484027fc4
moh <muhammad.qasim1@live.com>
2022-11-22 05:43:24 -0500

    fixed linting issues

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -38,7 +38,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item" //
-        src={item.image || fallbackimage}
+        src={ item.image || fallbackimage }
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
